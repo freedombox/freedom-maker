2031e4b51339ca9e15f4e736a47f979669134094
Bdale Garbee <bdale@gag.com>
2011-07-24 16:52:44 +0200

    working process including copying USB stick contents to internal microSD

--- bin/copy2dream.sh
+++ bin/copy2dream.sh
@@ -11,8 +11,19 @@ umount /media
 
 mke2fs -j /dev/sda2
 mount /dev/sda2 /media
-(cd / ; tar cf - `/bin/ls | grep -v proc | grep -v sys | grep -v media`) | \
+(cd / ; tar cf - `/bin/ls | grep -v proc | grep -v sys | grep -v media | grep -v dev`) | \
 	(cd /media ; tar xvf -)
+
+mkdir /media/proc /media/sys /media/media
+
+echo "Creating basic device nodes"
+mkdir /media/dev
+mknod /media/dev/console c 5 1
+mknod /media/dev/random c 1 8
+mknod /media/dev/urandom c 1 9
+mknod /media/dev/null c 1 3
+mknod /media/dev/ptmx c 5 2
+
 umount /dev/sda2
 
 echo "interrupt the next boot and change the root path to /dev/sda2"
