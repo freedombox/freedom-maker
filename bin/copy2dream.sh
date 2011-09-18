1e9268ec0234831f0f91421c058349188c4b0a83
Bdale Garbee <bdale@gag.com>
2011-09-18 11:40:05 -0600

    switch back to tar in copy2dream, as rsync is just too slow

--- bin/copy2dream.sh
+++ bin/copy2dream.sh
@@ -6,25 +6,27 @@
 #
 
 # partition microSD card inside DreamPlug
+echo "=> partition internal microSD card"
 parted -s /dev/sda mklabel msdos
 parted -s /dev/sda mkpart primary fat16 0 128
 parted -s /dev/sda mkpart primary ext2 128 100%
 
 # create filesystems on new partitions
+echo "=> create filesystems on internal microSD card"
 mkdosfs /dev/sda1
 mke2fs -j /dev/sda2
 
+echo "=> mount target partitions"
 mount /dev/sda2 /media
 mkdir -p /media/boot
 mount /dev/sda1 /media/boot
 
-rsync -atvz --progress --exclude=boot --exclude proc --exclude sys --exclude media --exclude dev / /media/freedom/
-
+echo "=> copy filesystem content from USB stick to target partitions"
+(cd / ; tar cf - `/bin/ls | grep -v boot | grep -v proc | grep -v sys | grep -v media | grep -v dev`) | (cd /media ; tar xvf -)
 cp /boot/* /media/boot/
-mkdir /media/proc /media/sys /media/media
 
-echo "Creating basic device nodes"
-mkdir /media/dev
+echo "=> touch up target root partition"
+mkdir /media/proc /media/sys /media/media /media/dev
 mknod /media/dev/console c 5 1
 mknod /media/dev/random c 1 8
 mknod /media/dev/urandom c 1 9
@@ -34,8 +36,9 @@ mknod /media/dev/ptmx c 5 2
 # patch up /etc/fstab entry for /boot
 sed -e 's/sdc1/sda1/g' < /etc/fstab > /media/etc/fstab
 
+echo "unmount target partitions"
 umount /dev/sda1
 umount /dev/sda2
 
-echo "installation complete .. see docs for how to boot from internal microSD"
+echo "=> installation complete, see docs to boot from internal microSD"
 
